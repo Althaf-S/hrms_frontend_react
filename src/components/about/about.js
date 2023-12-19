060ac06a160e47e74a7d612c6400ba4c9b1fbbbf
Althaf S <althaf.sabsara@gmail.com>
2023-12-19 22:10:31 +0530

    Edits About page

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,7 +1,43 @@
 export default function About(){
     return(
     <div>
-        <h1>This is a website dedicated for developing hrms application</h1>
-    </div>
+        <p>
+        Welcome to our Employee Management System, an application designed to streamline the way you oversee and interact with your workforce. <br/>Our platform empowers you with comprehensive insights into your team, facilitating efficient management.
+        </p>
+
+<h1>Key Features</h1>
+<p>
+<p>
+<strong>Employee Directory:</strong> Explore a detailed list of all your team members, organized for quick and easy access.
+</p>
+<p>
+<strong>Employee Profiles:</strong> Gain in-depth knowledge about each employee, including leave history, maximum leaves allowed, current position, and contact details.
+</p>
+<p>
+
+<strong>Leave Management:</strong> Seamlessly manage employee leaves. Our system provides a user-friendly interface to submit and track leaves.
+</p>
+</p>
+
+<h2>How It Works</h2>
+
+<p>
+    <p>
+<strong>Employee List:</strong> Browse through the complete list of employees, each with a dedicated profile.
+</p>
+<p>
+<strong>Employee Details:</strong> Click on an employee to view a detailed profile, including leave history, maximum leaves, position, and contact details.
+</p>
+<p>
+<strong>Leave Submission:</strong> Add leaves of employees directly through the platform, providing a streamlined process for management.
+</p>
+
+</p>
+
+</div>
+
+
+
+     
     )
 }
